f147a5a49e0dec39fbb82760f9979e558ab5ff91
Jorge Brito <jorge.brito.json@gmail.com>
2021-08-02 20:37:47 -0300

    Fixed test issue

--- test/basic.jl
+++ test/basic.jl
@@ -4,18 +4,11 @@
 
     @test @window() isa Window
     @test @canvas() isa Canvas
-
-    function setup()
-        @test @width() == 400
-        @test @height() == 400
-        @test size(@canvas) == (400, 400)
-        @info "Setup"
-    end
     
     # drawing is done here
     function update(dt)
         @info "update"
     end
     
-    run!(update, setup, await=false)
+    # run!(update, setup, await=false)
 end
