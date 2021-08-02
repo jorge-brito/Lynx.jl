c3663760b16053f7831c16973ecafda26104c0ef
Jorge Brito <jorge.brito.json@gmail.com>
2021-08-01 22:38:06 -0300

    New example

--- examples/basic.jl
+++ examples/basic.jl
@@ -1,34 +1,19 @@
 using Lynx
 using Luxor
 
-function App()
-    offset = [0., 1000.]
-    points = Point[]
+Lynx.init("Hello, world!", 400, 400)
 
-    function update(dt, canvas)
-        background("black")
-        sethue("yellowgreen")
+t = 0
 
-        x = noise(offset[1]) * canvas.width
-        y = noise(offset[2]) * canvas.height
-        
-        circle(x, y, 4, :fill)
-        push!(points, Point(x, y))
-        poly(points, :stroke)
-
-        offset .+= 2dt
-
-        if length(points) > 50
-            # remove the first point
-            splice!(points, 1)
-        end
-    end
-
-    Window("Basic example", 800, 600,
-        Paned(:v, position = 520,
-            Canvas(update)
-        ) # Paned
-    ) # Window
+# drawing is done here
+function update(dt)
+    background("#111")
+    origin()
+    sethue("yellowgreen")
+    circle(O, 50(cos(t) + 1), :fill)
+    global t += dt
 end
 
-Lynx.showall(App())
+# await=true will make sure the program only
+# stops when the window is closed
+run!(update, await=true)
