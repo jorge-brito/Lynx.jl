56f819cf24a617b7b9c9489dbbbd35bbecc61426
Jorge Brito <jorge.brito.json@gmail.com>
2021-08-14 11:29:47 -0300

    Removed files

--- src/gfunctions.jl
+++
@@ -1,9 +0,0 @@
-@gfunction gtk_widget_add_tick_callback(
-    @ptr(widget::GObject),
-    @ptr(callback::Any, Nothing),
-    @ptr(user_data::Any, Nothing),
-    @ptr(notify::Any, Nothing)
-) = Cuint
-
-@gfunction gtk_widget_remove_tick_callback(@ptr(widget::GObject), id::Cuint) = Nothing
-
